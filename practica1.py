466495b27570922511fec47e9ea269ffb216db40
acobo2020 <a.cobo.2020@alumnos.urjc.es>
2021-10-06 20:22:36 +0200

    jugando con las frecuencias y ciclos

--- practica1.py
+++ practica1.py
@@ -1,7 +1,8 @@
+
 #importamos la biblioteca RPi:
 import RPi.GPIO as GPIO
-
-#indicanos que esquema de numeracion de pin vamos a usar: numeracin de pin f´ısico (BOARD) o
+import time
+#indicanos que esquema de numeracion de pin vamos a usar: numeracin de pin fisicoo (BOARD) o
 #numeracion segun Broadcom SOCchannel (BCM)
 GPIO.setmode (GPIO.BOARD)
 
@@ -9,15 +10,30 @@ GPIO.setmode (GPIO.BOARD)
 GPIO.setup (11, GPIO.OUT)
 
 #creamos un objeto PWM con el pin a usar y la frecuencia de trabajo como parametros:
-pwm = GPIO.PWM (11,100)
+pwm = GPIO.PWM(11,100)
 
 #establecemos el ciclo de trabajo o DutyCyle (perıodo en que la senal esta en estado Alto)
 pwm.start (50)
-#si quisieseos cambiar el ciclo de trabajo:pwm.ChangeDutyCycle (ciclodetrabajodeseado)
-#si quisiesemos cambiar la frecuencia de la señal:pwm.ChangeFrequency (frecuenciadeseada)
-#para qui diese tiempo a hacerse estos cambios seria necesario usar un sleep o bien usando cualquier instruccion
+
+#cambiamos la frecuencia de la señal(esta limitada en 100)
+pwm.ChangeFrequency(50)
+#para que de tiempo a hacerse los cambios de frecuencia o ciclo es necesario usar un sleep o bien usando cualquier instruccion
 #que fuerce a parar el programa como:input("Ejecutando hasta que pulse una tecla...")
 input("Ejecutando hasta que pulse una tecla...")
+
+frecuencia = 5
+while frecuencia < 50:
+    #cambiamos el ciclo de trabajo
+    pwm.ChangeDutyCycle(frecuencia)
+    frecuencia+=5
+    time.sleep(0.5)
+    print(frecuencia)
+
+while frecuencia > 0:
+    pwm.ChangeDutyCycle(frecuencia)
+    frecuencia-=5
+    time.sleep(0.5)
+    print(frecuencia)
 
 #desactivamos el PWM con:
 pwm.stop ()
